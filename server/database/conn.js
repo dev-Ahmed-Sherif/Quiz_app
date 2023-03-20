97a941b990ab756a70ff80539c77384ad72ff83b
dev-Ahmed-Sherif <ahmedsherif91101@gmail.com>
2023-03-20 14:02:07 +0200

    "add Database conn"

---
+++ server/database/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery",false);
+
+mongoose.connect(process.env.DATABASE_CON , {useNewUrlParser : true})
+
+const db = mongoose.connection;
+
+db.on("error" , (error) => console.log(error))
+db.once("open" , () => console.log("DB connection is opened"))
--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const app = express();
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 
+import { config } from 'dotenv';
+
+config()
+
 app.listen(3030,()=>{
     console.log('server on 3030')
 })
