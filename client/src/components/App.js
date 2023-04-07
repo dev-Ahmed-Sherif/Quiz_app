f6b6622554b80da5dcf4bae92db39013073539a3
AhmedSherifAli <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-04-07 14:41:48 +0200

    Add quiz page 7 4 2023

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,41 @@
-import '../styles/App.css';
+import "../styles/App.css";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 /** import components */
-import Main from './Main';
-import Quiz from './Quiz';
-import Result from './Result';
-import { CheckUserExist } from '../helper/helper';
-
+import Main from "./Main";
+import Quiz from "./Quiz";
+import Result from "./Result";
+import AddQuiz from "./Add-quiz";
+import { CheckUserExist } from "../helper/helper";
 
 /** react routes */
 const router = createBrowserRouter([
   {
-    path : '/',
-    element : <Main></Main>
+    path: "/",
+    element: <Main></Main>,
   },
   {
-    path : '/quiz',
-    element : <CheckUserExist><Quiz /></CheckUserExist>
+    path: "/add-quiz",
+    element: <AddQuiz></AddQuiz>,
   },
   {
-    path : '/result',
-    element : <CheckUserExist><Result /></CheckUserExist>
+    path: "/quiz",
+    element: (
+      <CheckUserExist>
+        <Quiz />
+      </CheckUserExist>
+    ),
   },
-])
+  {
+    path: "/result",
+    element: (
+      <CheckUserExist>
+        <Result />
+      </CheckUserExist>
+    ),
+  },
+]);
 
 function App() {
   return (
