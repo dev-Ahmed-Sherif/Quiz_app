b5b0b0ebdd892f8527f24914f23cabe68d040bb2
AhmedSherifAli <105856194+dev-Ahmed-Sherif@users.noreply.github.com>
2023-04-23 17:22:57 +0200

    front "23 4 2023"

--- server/api/models/quiz.js
+++ server/api/models/quiz.js
@@ -1,35 +1,36 @@
 const mongoose = require("mongoose");
 
 const quizSchema = new mongoose.Schema(
-    {
-        academicYearId:{
-            type:mongoose.Types.ObjectId,
-            ref:"academicyears",
-            required:true 
-        },
-        subjectId:{
-            type:mongoose.Types.ObjectId,
-            ref:"subjects",
-            required:true 
-        },
-        questionIds:[{
-            type:mongoose.Types.ObjectId,
-            ref:"questions",
-        }],
-        type:{
-            type:String,
-            required:true 
-        }
-        ,
-        dateAdded: {
-            type:String
-        }
+  {
+    academicYearId: {
+      type: mongoose.Types.ObjectId,
+      ref: "academicyears",
+      required: true,
     },
-    {
-        versionKey:false,
-        strict:false
-    }
-)
+    subjectId: {
+      type: mongoose.Types.ObjectId,
+      ref: "subjects",
+      required: true,
+    },
+    questionIds: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "questions",
+      },
+    ],
+    type: {
+      type: String,
+      required: true,
+    },
+    dateAdded: {
+      type: String,
+    },
+  },
+  {
+    versionKey: false,
+    strict: false,
+  }
+);
 
-quiz = mongoose.model("quizzes",quizSchema);
-module.exports = quiz
+quiz = mongoose.model("quizzes", quizSchema);
+module.exports = quiz;
